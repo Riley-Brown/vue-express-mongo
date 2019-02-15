848cd74db557a5be0ee8e08ffe39b54a7f2635de
Riley-Brown <riley@riley.gg>
2019-02-15 00:33:25 -0500

    Connected to mongoDB and created helper function to load posts

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,15 +1,25 @@
+require('dotenv').config();
 const express = require('express');
 const mongodb = require('mongodb');
 
 const router = express.Router();
 
 /* ===== GET POSTS ===== */
-router.get('/', (req, res) => {
-  res.send({ test: 'hello' });
+router.get('/', async (req, res) => {
+  const posts = await loadPostsCollection();
+  res.send(await posts.find({}).toArray());
 });
 
 /* ===== ADD POSTS ===== */
 
 /* ===== DELETE POSTS ===== */
+
+async function loadPostsCollection() {
+  const client = await mongodb.MongoClient.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true
+  });
+
+  return client.db('vue_express').collection('posts');
+}
 
 module.exports = router;
